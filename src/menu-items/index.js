1be06271309e6054069cc4052bc6d6e47d86be03
Parvez-Akhtar-codes <parvezakhtar310@gmail.com>
2024-09-01 16:31:49 +0530

    intial commit

---
+++ src/menu-items/index.js
@@ -0,0 +1,12 @@
+// project import
+import pages from './pages';
+
+import support from './support';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+  items: [ pages, support]
+};
+
+export default menuItems;
